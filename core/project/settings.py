d9024941d5e8a0747c6f8381160cecc0b2cc4c8d
sphiwe-mathe5 <nazireemathe@gmail.com>
2024-11-22 23:19:01 +0200

    3rd commit

--- core/project/settings.py
+++ core/project/settings.py
@@ -129,8 +129,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
